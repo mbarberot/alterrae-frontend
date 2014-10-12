8ad04ce85c97b99abc23622fb6878729d6619cce
Mathieu Barberot <mathieu.barberot@gmail.com>
2014-10-12 22:54:45 +0200

    [Fixture] Tweak for querying in fixtures

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,10 +1,30 @@
 import DS from 'ember-data';
 
-var ApplicationAdapter = DS.RESTAdapter.extend({});
-
-DS.RESTAdapter.reopen({
-    host: 'https://api.sistearth.com',
-    namespace: 'api/public'
+var ApplicationAdapter = DS.FixtureAdapter.extend({
+    queryFixtures: function(records, query) {
+        return records.filter(function(record) {
+            for(var key in query) {
+                if (!query.hasOwnProperty(key)) { continue; }
+                var value = query[key];
+                if (record[key] !== value) { return false; }
+            }
+            return true;
+        });
+    }
 });
+
+/*var ApplicationAdapter = DS.RESTAdapter.extend({});
+
+ DS.RESTAdapter.reopen({
+ host: 'https://api.sistearth.com',
+ namespace: 'api',
+ pathForType: function(type) {
+ if(type === 'post') {
+ return 'public/posts';
+ } else {
+ return type;
+ }
+ }
+ });*/
 
 export default ApplicationAdapter;
