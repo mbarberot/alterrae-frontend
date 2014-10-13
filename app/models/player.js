41db7bbb88db8ce1444802dc76549f6dd94db03a
Mathieu Barberot <mathieu.barberot@gmail.com>
2014-10-13 13:10:17 +0200

    [Fixture] Complete player model and add fixtures

--- app/models/player.js
+++ app/models/player.js
@@ -1,22 +1,30 @@
 import DS from "ember-data";
 
 var Player = DS.Model.extend({
-    user: DS.belongsTo('user')
+    sex: DS.attr('boolean'),
+    user: DS.belongsTo('user'),
+    kingdom: DS.attr('string')
 });
 
 Player.reopenClass({
     FIXTURES: [
         {
             id: 1,
-            user : "1"
+            user : "1",
+            sex: true,
+            kingdom: "Mizu"
         },
         {
             id: 2,
-            user: "2"
+            user: "2",
+            sex: false,
+            kingdom: "Tsuchi"
         },
         {
             id: 3,
-            user: "3"
+            user: "3",
+            sex: false,
+            kingdom: "Reikon"
         }
     ]
 });
