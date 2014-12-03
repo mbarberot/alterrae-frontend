316721553b5cfde1af19c7607a43c2c107928651
Mathieu Barberot <mathieu.barberot@gmail.com>
2014-12-03 14:45:00 +0100

    [#7|Register] Form validation
    - creating empty user in user route
    - auto-validating email and password confirmations
    - simplify action 'register'
    - display error message at the right of the input

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -1,27 +1,33 @@
 import Ember from 'ember';
 
 export
-default Ember.Controller.extend({
-    errorPassword: false,
-    errorEmail: false,
+default Ember.ObjectController.extend({
+    checkPassword: function () {
+        var password = this.get("password");
+        var passwordConfirmation = this.get("passwordConfirmation");
+        return password === passwordConfirmation;
+    }.property("model.password", "passwordConfirmation"),
+
+    checkEmail: function () {
+        var email = this.get("email");
+        var emailConfirmation = this.get("emailConfirmation");
+        return email === emailConfirmation;
+    }.property("model.email", "emailConfirmation"),
+
+    cannotSubmit: function () {
+        return !this.get("checkPassword") || !this.get("checkEmail");
+    }.property("checkPassword", "checkEmail"),
+
     actions: {
         register: function () {
-            var username = this.get('username');
-            var password = this.get('password');
-            var passwordConfirmation = this.get('passwordConfirmation');
-            var email = this.get('email');
-            var emailConfirmation = this.get('emailConfirmation');
-            this.errorPassword = (password !== passwordConfirmation);
-            this.errorEmail = (email !== emailConfirmation);
-            if (!this.errorEmail && !this.errorPassword) {
-                var user = this.store.createRecord('user', {
-                    username: username,
-                    password: password,
-                    email:email
-                });
-                console.log(user);
-                user.save();
-            }
+            var user = this.get('model');
+            var controller = this;
+            user.save().then(function () {
+                // todo display a success message before(/after?) redirection
+                controller.transitionToRoute('/login');
+            }, function () {
+                // todo handling errors
+            });
         }
     }
 });
