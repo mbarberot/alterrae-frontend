398a1e4da89ea2ec9fd19a8c548ffa3512d3890d
Mathieu Barberot <mathieu.barberot@gmail.com>
2014-12-20 12:40:53 +0100

    [#5] Upgrade dependencies
    - .jshintrc : from ``ember init`` + moment.js global def
    - .travis.yml : from ``ember init``
    - Brocfile.js : remove useless bootstrap icons import
    - index.html + test : from ``ember init``
    - bower.json : updated
    - environment.js : from ``ember init`` + reset broken simple-auth config
    - package.json : updated
    - robots.txt : from ``ember init``
    - start-app.js : from ``ember init``

--- config/environment.js
+++ config/environment.js
@@ -28,10 +28,6 @@ module.exports = function(environment) {
     }
   };
 
-  ENV['simple-auth'] = {
-    routeAfterAuthentication: '/game'
-  };
-
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     ENV.APP.LOG_ACTIVE_GENERATION = true;
@@ -43,11 +39,12 @@ module.exports = function(environment) {
   if (environment === 'test') {
     // Testem prefers this...
     ENV.baseURL = '/';
-    ENV.locationType = 'auto';
+    ENV.locationType = 'none';
 
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
+
     ENV.APP.rootElement = '#ember-testing';
   }
 
