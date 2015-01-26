5eaeeff1db1f85b3cfd681c93f2fef532c7860d9
Mathieu Barberot <mathieu.barberot@gmail.com>
2015-01-26 05:23:39 +0100

    [#2] API Login returns user id. Using it to find current user

--- app/routes/account.js
+++ app/routes/account.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin);
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+	model: function () {
+		var userId = this.get('session').get('content')['profile']['id'];
+		return this.get('store').find('user', userId);
+	}
+});
