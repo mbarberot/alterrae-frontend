51ce88ea19117b8ce9a2f3bb157bd26fac0dff5e
Mathieu Barberot <mathieu.barberot@gmail.com>
2015-01-31 21:54:42 +0100

    [#10] Using the lib

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,19 +1,16 @@
 import Ember from 'ember';
 import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
-import Configuration from 'simple-auth/configuration';
-
-Configuration.routeAfterAuthentication = 'game';
 
 export
-default Ember.Controller.extend(LoginControllerMixin, {
-    authenticator: 'authenticator:sistearth',
-    actions: {
-        authenticate: function () {
-            var _this = this;
-            this._super().then(null, function (error) {
-                var message = JSON.parse(error);
-                _this.set('errorMessage', message);
-            });
-        }
+  default Ember.Controller.extend(LoginControllerMixin, {
+  authenticator: 'simple-auth-authenticator:token',
+  actions: {
+    authenticate: function () {
+      var _this = this;
+      this._super().then(null, function (error) {
+        var message = JSON.parse(error);
+        _this.set('errorMessage', message);
+      });
     }
+  }
 });
