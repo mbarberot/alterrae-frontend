1e5da15c9867e7449a35ea43d08c663c583eee19
Mathieu Barberot <mathieu.barberot@gmail.com>
2015-01-31 21:40:53 +0100

    [#2] Using deleteSelf implementation

--- app/controllers/account.js
+++ app/controllers/account.js
@@ -1,28 +1,31 @@
 import Ember from 'ember';
 
 export
-default Ember.Controller.extend({
-    confirmed: false,
-    actions: {
-        deleteSelf: function () {
-            this.set('password', '');
-            this.set('confirmed', false);
-            var _this = this;
-            Ember.$('#modal-password-confirmation').modal('show')
-                .on('hidden.bs.modal', function () {
-                    if (_this.get('confirmed')) {
-                        _this.adapter().deleteSelf();
-                        _this.get('session').invalidate();
-                    }
-                });
-        },
-        confirmPassword: function () {
-            this.set('confirmed', true);
-        }
+  default Ember.Controller.extend({
+  confirmed: false,
+  actions: {
+    deleteSelf: function () {
+      this.set('password', '');
+      this.set('confirmed', false);
+      var _this = this;
+      Ember.$('#modal-password-confirmation').modal('show')
+        .on('hidden.bs.modal', function () {
+          if (_this.get('confirmed')) {
+            _this.adapter().deleteSelf(
+              _this.get('model'),
+              _this.get('password')
+            );
+            _this.get('session').invalidate();
+          }
+        });
     },
-
-    // API ''/users/self'' workaround
-    adapter: function() {
-        return this.get('store').adapterFor(this.get('model').constructor);
+    confirmPassword: function () {
+      this.set('confirmed', true);
     }
+  },
+
+  // API ''/users/self'' workaround
+  adapter: function () {
+    return this.get('store').adapterFor(this.get('model').constructor);
+  }
 });
