0de1e9e2a0c9ccc118f0640a63126f4fedf908e4
mbarberot <mathieu.barberot@gmail.com>
2015-04-24 16:16:12 +0200

    [#11|upgrade ember-cli] updating templates - index controller : seems like this obvious arraycontroller is needed to get  a controller in the template - post controller : remove useless authorName, reformat code - post model : reformat - templates : update with new syntax

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -1,37 +1,27 @@
 import Ember from "ember";
 
 export
-default Ember.ObjectController.extend({
-    accordionHref: function (key, value) {
-        var model = this.get('model');
+  default Ember.Controller.extend({
+  accordionHref: function (key, value) {
+    var model = this.get('model');
 
-        if (value === undefined) {
-            return "#post-" + model.get('id');
-        }
-    }.property('model.id'),
+    if (value === undefined) {
+      return "#post-" + model.get('id');
+    }
+  }.property('model.id'),
 
-    accordionId: function (key, value) {
-        var model = this.get('model');
+  accordionId: function (key, value) {
+    var model = this.get('model');
 
-        if (value === undefined) {
-            return "post-" + model.get('id');
-        }
-    }.property('model.id'),
+    if (value === undefined) {
+      return "post-" + model.get('id');
+    }
+  }.property('model.id'),
 
-    formattedDate: function (key, value) {
-        var model = this.get('model');
-
-        if (value === undefined) {
-            return moment(model.get('created_at')).format('LL');
-        }
-    }.property('model.created_at'),
-
-    authorName: function (key, value) {
-        var model = this.get('model');
-        if (value === undefined) {
-            return model.get('author.username').capitalize();
-        }
-    }.property('model.author.username')
-
-
+  formattedDate: function (key, value) {
+    var model = this.get('model');
+    if (value === undefined) {
+      return moment(model.get('created_at')).format('LL');
+    }
+  }.property('model.created_at')
 });
