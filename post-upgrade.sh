6e3b4494e0aeaef744ce745d435145903ffcac20
mbarberot <mathieu.barberot@gmail.com>
2015-06-22 20:58:54 +0200

    [update] Updating ember-cli from 0.2.3 to 0.2.7 - update templates

--- post-upgrade.sh
+++ post-upgrade.sh
@@ -7,15 +7,17 @@ npm install --save broccoli-static-compiler
 echo "(Re)Installing ember-cli plugins"
 
 npm install --save-dev ember-cli-bootstrap-sass
+npm install --save-dev ember-cli-simple-auth-token
 npm install --save-dev ember-validations
 
 echo "Running 'ember generate' commands"
 
 ember install ember-cli-simple-auth
 ember install ember-cli-simple-auth-testing
-ember install simple-auth-token
 ember install ember-faker
 ember install ember-moment
+
+ember generate simple-auth-token
 
 echo "npm install"
 
