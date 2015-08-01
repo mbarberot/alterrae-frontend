cde16b1301b37250c388a907f20fe22a642717e3
mbarberot <mathieu.barberot@gmail.com>
2015-08-01 22:14:14 +0200

    [#17] Removing log

--- app/mixins/player-route-mixin.js
+++ app/mixins/player-route-mixin.js
@@ -7,9 +7,7 @@ export default Ember.Mixin.create(AuthenticatedRouteMixin, {
     var superResult = this._super(transition);
 
     var roles = this.get(Configuration.sessionPropertyName).get('content').secure.profile.roles;
-
     if (!roles.contains("ROLE_PLAYER")) {
-      console.log("User has no player | roles -> ", roles);
       transition.abort();
       this.transitionTo('/player/create');
     }
