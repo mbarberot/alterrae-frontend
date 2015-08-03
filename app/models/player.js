78e082a0ceda70992e2eced63e647d66dd2139a1
Mathieu Barberot <mathieu.barberot@gmail.com>
2015-08-03 16:48:12 +0200

    [#14] Completing model + reformating

--- app/models/player.js
+++ app/models/player.js
@@ -1,33 +1,9 @@
 import DS from "ember-data";
 
-var Player = DS.Model.extend({
-    sex: DS.attr('boolean'),
-    user: DS.belongsTo('user'),
-    kingdom: DS.attr('string')
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  sex: DS.attr('boolean'),
+  class: DS.attr('string'),
+  user: DS.belongsTo('user'),
+  kingdom: DS.attr('string')
 });
-
-Player.reopenClass({
-    FIXTURES: [
-        {
-            id: 1,
-            user : "1",
-            sex: true,
-            kingdom: "Mizu"
-        },
-        {
-            id: 2,
-            user: "2",
-            sex: false,
-            kingdom: "Tsuchi"
-        },
-        {
-            id: 3,
-            user: "3",
-            sex: false,
-            kingdom: "Reikon"
-        }
-    ]
-});
-
-export
-    default Player;
