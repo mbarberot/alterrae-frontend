6e7fd15c2a4593757c257a538232c5c3a0f137a4
Mathieu Barberot <mathieu.barberot@gmail.com>
2015-10-09 16:50:11 +0200

    [java-backend] Renaming

--- app/models/post.js
+++ app/models/post.js
@@ -3,7 +3,7 @@ import DS from "ember-data";
 export
   default DS.Model.extend({
   title: DS.attr('string'),
-  text: DS.attr('string'),
+  body: DS.attr('string'),
   created_at: DS.attr('string'),
   author: DS.belongsTo('user', {
     async: true
